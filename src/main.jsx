2b94355c7b77fcbe0c29a53fa5057e73c6ae75b6
yppelu <yppelu@gmail.com>
2024-01-17 10:47:26 +0700

    Add basic styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import './styles/main.css';
+
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
