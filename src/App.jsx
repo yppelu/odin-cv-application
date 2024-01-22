98cc8932082a5e656f5de83bcbbf266b865e94f1
yppelu <yppelu@gmail.com>
2024-01-22 11:34:33 +0700

    Add a printing button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import emptyData from './helpers/empty-data.js';
 
+import PrintingButton from './components/printing-button/PrintingButton.jsx';
 import Editor from './components/editor/Editor.jsx';
 import Resume from './components/resume/Resume.jsx';
 
@@ -10,6 +11,32 @@ function App() {
 
   return (
     <>
+      <style>
+        {`
+          @media print {
+            body{
+              -webkit-print-color-adjust:exact !important;
+              print-color-adjust:exact !important;
+            }
+            #root {
+              padding: 0;
+              margin: 0;
+              width: 100%;
+            }
+            .editor {
+              display: none;
+            }
+            .printing-button {
+              display: none;
+            }
+            .resume {
+              box-shadow: none;
+              width: 100%;
+            }
+          }
+        `}
+      </style>
+      <PrintingButton />
       <Editor person={person} setPerson={setPerson} />
       <Resume person={person} />
     </>
